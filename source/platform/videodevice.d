c61d5f6cf065dd0700422af0749a5d93c78d683e
Ferdinand Majerech <kiithsacmp@gmail.com>
2014-08-11 00:16:22 +0200

    A simple video device wrapping SDL window and GL context.

---
+++ source/platform/videodevice.d
@@ -0,0 +1,130 @@
+//          Copyright Ferdinand Majerech 2014.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+/// Manages windowing and GL and provides access to convenience OpenGL wrappers.
+module platform.videodevice;
+
+
+import std.exception;
+import std.logger;
+import std.typecons;
+
+import derelict.opengl3.gl3;
+import derelict.sdl2.sdl;
+
+
+import gfmod.opengl.opengl;
+
+
+/// Manages windowing and GL and provides access to convenience OpenGL wrappers.
+class VideoDevice
+{
+private:
+    // The game log.
+    Logger log_;
+
+    // OpenGL wrapper managing GL versions and information.
+    OpenGL gl_;
+
+    // The main game window.
+    SDL_Window* window_;
+
+    // OpenGL context.
+    SDL_GLContext context_;
+
+public:
+    /**
+     * Construct a VideoDevice with specified OpenGL drawing to window.
+     *
+     * Note that to fully construct a VideoDevice you also need to call initWindow()
+     * and initGL().
+     *
+     * Params:
+     *
+     * log    = The game log.
+     */
+    this(Logger log) @safe pure nothrow @nogc
+    {
+        log_    = log;
+    }
+
+    /// Destroy the VideoDevice. Must be called by the user (use e.g. scoped!).
+    ~this()
+    {
+        if(gl_ !is null)                   { destroy(gl_); }
+        if(context_ != SDL_GLContext.init) { SDL_GL_DeleteContext(context_); }
+        if(window_ !is null)               { SDL_DestroyWindow(window_); }
+    }
+
+    import platform.sdl2;
+    /**
+     * Initialize the GL window.
+     *
+     * Must be called before initGL.
+     */
+    bool initWindow(size_t width, size_t height, Flag!"fullscreen" fullscreen)
+        @trusted nothrow
+    {
+        assert(window_ is null, "Double initialization of the main window");
+        window_ = createGLWindow(width, height, fullscreen);
+
+        // Exit if window creation fails.
+        if(null is window_)
+        {
+            log_.critical("Failed to create the application window").assumeWontThrow;
+            return false;
+        }
+        log_.infof("Created a%s window with dimensions %s x %s",
+                   fullscreen ? " fullscreen" : "", width, height).assumeWontThrow;
+        return true;
+    }
+
+    /**
+     * Initialize the OpenGL context.
+     *
+     * Returns: true on success, false on failure.
+     */
+    bool initGL() @trusted nothrow
+    {
+        assert(window_ !is null, "Can't initialize GL without an initialized window");
+        assert(gl_ is null, "Double initialization of GL");
+
+        SDL_GLContext context;
+        try
+        {
+            gl_      = new OpenGL(log_);
+            context_ = SDL_GL_CreateContext(window_);
+            if(gl_.reload() < GLVersion.GL30)
+            {
+                log_.critical("Required OpenGL version 3.0 could not be loaded.");
+                return false;
+            }
+        }
+        catch(OpenGLException e)
+        {
+            log_.critical("Failed to initialize OpenGL: ", e).assumeWontThrow;
+            return false;
+        }
+        catch(Exception e)
+        {
+            assert(false, "Unexpected exception in initGL: " ~ e.msg);
+        }
+        return true;
+    }
+
+    /// Get access to the OpenGL API.
+    OpenGL gl() @safe pure nothrow @nogc 
+    {
+        assert(gl_ !is null, "Trying to access GL before it is initialized");
+        return gl_;
+    }
+
+    /// Swap the front and back buffer at the end of a frame.
+    void swapBuffers() @trusted nothrow
+    {
+        assert(window_ !is null, "Can't swap buffers without an initialized window");
+        SDL_GL_SwapWindow(window_);
+    }
+}
