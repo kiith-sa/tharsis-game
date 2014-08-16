8a432891d6d797d90d7688b623cc7875395b382f
Ferdinand Majerech <kiithsacmp@gmail.com>
2014-08-17 00:31:56 +0200

    Game updates are now regulated by GameTime.

--- source/game/mainloop.d
+++ source/game/mainloop.d
@@ -26,24 +26,32 @@ import time.gametime;
 bool mainLoop(ref EntitySystem entitySystem, VideoDevice video, InputDevice input,
               GameTime time, Logger log) @trusted nothrow
 {
-    entitySystem.spawnEntityASAP("game_data/entity1.yaml");
+    entitySystem.spawnEntityASAP("game_data/level1.yaml");
 
     for(;;)
     {
-        input.collectInput();
-        if(input.quit) { return true; }
+        // TODO: measure time taken by an update (iteration of this while loop) 
+        // Instead of an FPS display, have a 'Load' display, where 100% is timeStep
+        // and 0% is 0. 2014-08-16
+        while(time.timeToUpdate())
+        {
+            input.collectInput();
+            if(input.quit) { return true; }
 
-        import derelict.opengl3.gl3;
-        // Clear the back buffer with a red background (parameters are R, G, B, A)
-        glClearColor(0.01, 0.01, 0.04, 1.0);
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+            import derelict.opengl3.gl3;
+            // Clear the back buffer with a red background (parameters are R, G, B, A)
+            glClearColor(0.01, 0.01, 0.04, 1.0);
+            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-        entitySystem.frame();
-        // Log GL errors, if any.
-        video.gl.runtimeCheck();
+            entitySystem.frame();
+            // Log GL errors, if any.
+            video.gl.runtimeCheck();
 
-        // Swap the back buffer to the front, showing it in the window.
-        video.swapBuffers();
+            // Swap the back buffer to the front, showing it in the window.
+            video.swapBuffers();
+
+            time.finishedUpdate();
+        }
     }
 
     assert(false, "This should never be reached");
