286dde3fbd8ff9dcea0d90466917f002515b3893
Ferdinand Majerech <kiithsacmp@gmail.com>
2014-08-21 01:22:44 +0200

    EntitySystem now constructs Camera.

--- source/entity/entitysystem.d
+++ source/entity/entitysystem.d
@@ -72,7 +72,7 @@ public:
                                                   VisualComponent,
                                                   SpawnerMultiComponent,
                                                   TimedTriggerMultiComponent);
-    
+
         componentTypeMgr_.lock();
 
         entityMgr_ = new DefaultEntityManager(componentTypeMgr_);
@@ -80,11 +80,13 @@ public:
         prototypeMgr_ = new PrototypeManager(componentTypeMgr_, entityMgr_);
 
         import tharsis.defaults.copyprocess;
+        import game.camera;
+        auto camera = new Camera(video.width, video.height);
         
         auto dummyPosition = new CopyProcess!PositionComponent();
         auto dummyVisual   = new CopyProcess!VisualComponent();
         auto dummyLife     = new CopyProcess!LifeComponent();
-        renderer_          = new RenderProcess(video, log);
+        renderer_          = new RenderProcess(video, camera, log);
 
         auto conditionProc = new TimedTriggerProcess(&time.timeStep);
         auto spawner = new DefaultSpawnerProcess(&entityMgr_.addEntity, prototypeMgr_,
