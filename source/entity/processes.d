94b126a09a757a379c971d7b2074ef01c3ab8802
Ferdinand Majerech <kiithsacmp@gmail.com>
2014-08-25 22:22:44 +0200

    Added a SpawnerProcess with support for weapons.

--- source/entity/processes.d
+++ source/entity/processes.d
@@ -620,3 +620,111 @@ public:
     }
 }
 
+
+import tharsis.defaults.processes;
+import tharsis.entity.entitypolicy;
+/** A SpawnerProcess with support for weapons.
+ *
+ * Treats weapons as spawn conditions. Spawns spawner components matching a weapon when
+ * $(D secsTillBurst) of that weapon is negative. SpawnerAttachProcess adds spawner
+ * components to spawn projectiles of each weapon in an entity. $(D triggerID) of those
+ * spawner components is entity.components.minWeaponTriggerID + index of the weapon that
+ * fires the projectile. The index points to weapon (multi) components of the entity.
+ */
+final class WeaponizedSpawnerProcess: DefaultSpawnerProcess
+{
+private:
+    // Game log.
+    Logger log_;
+
+    import tharsis.defaults.components;
+    import tharsis.entity.componenttypemanager;
+    import tharsis.entity.entitymanager;
+    import tharsis.entity.entityprototype;
+    import tharsis.entity.resourcemanager;
+
+public:
+    /** Construct a WeaponizedSpawnerProcess.
+     *
+     * Params:
+     *
+     * addEntity            = Delegate to add an entity.
+     * prototypeManager     = Manages entity prototype resources.
+     * componentTypeManager = The component type manager where all used component types
+     *                        are registered.
+     * log                  = The game log.
+     *
+     * Examples:
+     * --------------------
+     * // EntityManager entityManager
+     * // ResourceManager!EntityPrototypeResource prototypeManager
+     * // ComponentTypeManager componentTypeManager
+     * // Logger log
+     * auto spawner = new WeaponizedSpawnerProcess(&entityManager.addEntity, prototypeManager,
+     *                                             componentTypeManager, log);
+     * --------------------
+     */
+    this(DefaultSpawnerProcess.AddEntity addEntity,
+         ResourceManager!EntityPrototypeResource prototypeManager,
+         AbstractComponentTypeManager componentTypeManager,
+         Logger log)
+        @safe pure nothrow
+    {
+        log_ = log;
+        super(addEntity, prototypeManager, componentTypeManager);
+    }
+
+    /// Shortcut alias.
+    alias Context = DefaultEntityManager.Context;
+
+    /** A simple forward for SpawnerProcess process(). 
+     * 
+     * Needed because user code doesn't see SpawnerProcess.process() for some reason,
+     * probably a DMD bug (as of DMD 2.066).
+     */
+    override void process(ref const(Context) context,
+                 immutable SpawnerMultiComponent[] spawners,
+                 immutable TimedTriggerMultiComponent[] triggers) nothrow
+    {
+        super.process(context, spawners, triggers);
+    }
+
+    /** Reads spawners and weapons.
+     *
+     * If a weapon fires ($(D weapon.secsTillBurst < 0)), spawns projectiles of that
+     * weapon.
+     */
+    void process(ref const(Context) context,
+                 immutable SpawnerMultiComponent[] spawners,
+                 immutable WeaponMultiComponent[] weapons) nothrow
+    {
+        // Find triggers matching this spawner component, and spawn if found.
+        outer: foreach(ref spawner; spawners) foreach(size_t idx, ref weapon; weapons)
+        {
+            const weaponID = idx + minWeaponTriggerID;
+            // Weapon trigger must match the spawner component's trigger ID.
+            if(weaponID != spawner.triggerID) { continue; }
+
+            // If the spawner is not fully loaded (any of its resources not in the
+            // Loaded state), ignore it completely and move on to the next one. This
+            // means we miss spawns when a spawner is not loaded. We may add 'delayed'
+            // spawns to compensate for this in future.
+            if(!spawnerReady(spawner)) { continue outer; }
+
+            // We've not reached the time to spawn yet.
+            if(weapon.secsTillBurst <= 0.0f) { spawn(context, spawner); }
+        }
+    }
+
+    /// Process an entity with both weapons and timed triggers.
+    void process(ref const(Context) context,
+                 immutable SpawnerMultiComponent[] spawners,
+                 immutable WeaponMultiComponent[] weapons,
+                 immutable TimedTriggerMultiComponent[] triggers) nothrow
+    {
+        // Delegate to process() overloads handling individual triggers
+        // (weapons, timed triggers ATM).
+        process(context, spawners, weapons);
+        process(context, spawners, triggers);
+    }
+}
