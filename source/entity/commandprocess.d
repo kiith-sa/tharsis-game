34ea61ead3cf7602a74830458338b75d56917b8c
Ferdinand Majerech <kiithsacmp@gmail.com>
2014-08-29 03:19:36 +0200

    Moved CommandProcess, WeaponizedSpawnerProcess to separate modules.

---
+++ source/entity/commandprocess.d
@@ -0,0 +1,164 @@
+//          Copyright Ferdinand Majerech 2014.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+/// Process that assigns commands (e.g. move to point) to individual entities.
+module entity.commandprocess;
+
+
+import std.exception;
+import std.logger;
+
+import gl3n_extra.linalg;
+
+import entity.components;
+import game.camera;
+
+
+/// Process that assigns commands (e.g. move to point) to individual entities.
+final class CommandProcess
+{
+private:
+    // Game log.
+    Logger log_;
+
+    import platform.inputdevice;
+    // Access to mouse input.
+    const(Mouse) mouse_;
+    // Access to keyboard input.
+    const(Keyboard) keyboard_;
+    // Camera to transform mouse coords to world space.
+    const(Camera) camera_;
+
+    // If true, there is no command for selected units this frame.
+    bool noCommandForSelected_ = true;
+
+    // Command we're about to give to selected entities.
+    CommandComponent commandForSelected_;
+
+    // TODO: closeEnough should be in YAML for CommandProcess 2014-08-20
+    // If an entity with a MoveTo command is at least this close to its target, there's
+    // no need to continue moving.
+    enum closeEnough = 67.0f;
+
+public:
+    alias FutureComponent = CommandComponent;
+
+    /** Construct a CommandProcess.
+     *
+     * Params:
+     *
+     * mouse  = Access to mouse input.
+     * camera = Camera to transform mouse coords to world space.
+     * log    = Game log.
+     */
+    this(const(Mouse) mouse, const(Keyboard) keyboard, const(Camera) camera, Logger log)
+        @safe pure nothrow @nogc
+    {
+        log_      = log;
+        mouse_    = mouse;
+        keyboard_ = keyboard;
+        camera_   = camera;
+    }
+
+    /// Determine which commands to give this frame.
+    void preProcess() nothrow
+    {
+        noCommandForSelected_ = true;
+        immutable mousePos = vec2(mouse_.x, mouse_.y);
+
+        /* Get the point on the map where the mouse cursor points.
+         *
+         * Writes the point to mapPoint. Returns false if there is no such point, true
+         * otherwise.
+         */
+        bool mouseOnMap(out vec3 mapPoint) @safe pure nothrow @nogc
+        {
+            import gl3n_extra.plane;
+            // For now we assume the map is one big flat plane.
+            const mapPlane = planeFromPointNormal(vec3(0, 0, 0), vec3(0, 0, 1));
+
+            // Create a line in world space from a point on the screen.
+            const linePoint1 = camera_.screenToWorld(vec3(mousePos, 0.0f));
+            const linePoint2 = camera_.screenToWorld(vec3(mousePos, -100.0f));
+            const lineVector = linePoint2 - linePoint1;
+
+            // Intersect the line with the map to find the point where the mouse id.
+            return mapPlane.intersectsLine(linePoint1, lineVector, mapPoint);
+
+            // TODO: Once we have terrain, we'll need to generate the 'walkability' map
+            // and intersect with that. 2014-08-21
+            // TODO: Once we have a walkability map, use flow fields for pathfinding. 2014-08-19
+        }
+
+        // Right click means 'move to' by default, 'fire at without moving' if ctrl is
+        // pressed (Of course this will change later as commands get more advanced).
+        vec3 mapPoint;
+        if(mouse_.clicked(Mouse.Button.Right) && mouseOnMap(mapPoint))
+        {
+            noCommandForSelected_ = false;
+            if(keyboard_.key(Key.LCtrl))
+            {
+                commandForSelected_.type         = CommandComponent.Type.StaticFireAt;
+                commandForSelected_.staticFireAt = mapPoint;
+            }
+            else
+            {
+                commandForSelected_.type   = CommandComponent.Type.MoveTo;
+                commandForSelected_.moveTo = mapPoint;
+            }
+        }
+    }
+
+    // TODO: In future there will be other sources of commands than just mouse
+    //       (AI, map scripts, waypoints, etc.) 2014-08-21
+    /** Add a command component to an entity that doesn't have one.
+     *
+     * We only give movement commands to entities that have engines.
+     */
+    void process(ref const PositionComponent pos,
+                 ref const SelectionComponent select,
+                 ref const EngineComponent engine,
+                 ref CommandComponent* command) nothrow
+    {
+        if(noCommandForSelected_)
+        {
+            command = null;
+            return;
+        }
+        *command = commandForSelected_;
+    }
+
+    /// Update (or cancel) command of an entity that already has a command assigned.
+    void process(ref const PositionComponent pos,
+                 ref const CommandComponent commandPast,
+                 ref CommandComponent* commandFuture) nothrow
+    {
+        with(CommandComponent.Type) final switch(commandPast.type)
+        {
+            case MoveTo:
+                if(distance(commandPast.moveTo, vec3(pos.x, pos.y, pos.z)) < closeEnough)
+                {
+                    commandFuture = null;
+                    break;
+                }
+                *commandFuture = commandPast;
+                break;
+            case StaticFireAt:
+                *commandFuture = commandPast;
+                break;
+        }
+    }
+
+    /// Handle an entity that is both selected and has a command already.
+    void process(ref const PositionComponent pos,
+                 ref const SelectionComponent select,
+                 ref const EngineComponent engine,
+                 ref const CommandComponent commandPast,
+                 ref CommandComponent* commandFuture) nothrow
+    {
+        noCommandForSelected_ ? process(pos, commandPast, commandFuture)
+                              : process(pos, select, engine, commandFuture);
+    }
+}
---
+++ source/entity/weaponizedspawnerprocess.d
@@ -0,0 +1,166 @@
+//          Copyright Ferdinand Majerech 2014.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+/// A SpawnerProcess with support for weapons.
+module entity.weaponizedspawnerprocess;
+
+import std.exception;
+import std.logger;
+
+import gl3n_extra.linalg;
+import tharsis.defaults.processes;
+import tharsis.entity.entitypolicy;
+
+import entity.components;
+
+
+/** A SpawnerProcess with support for weapons.
+ *
+ * Treats weapons as spawn conditions. Spawns spawner components matching a weapon when
+ * $(D secsTillBurst) of that weapon is negative. SpawnerAttachProcess adds spawner
+ * components to spawn projectiles of each weapon in an entity. $(D triggerID) of those
+ * spawner components is entity.components.minWeaponTriggerID + index of the weapon that
+ * fires the projectile. The index points to weapon (multi) components of the entity.
+ */
+final class WeaponizedSpawnerProcess: DefaultSpawnerProcess
+{
+private:
+    // Game log.
+    Logger log_;
+
+    // Pointer to the currently processed weapon component.
+    //
+    // Only set while spawning a projectile entity, must be null otherwise.
+    //
+    // process() reads weapon components to determine when to spawn projectile entities.
+    // When it spawns a projectile, we still need to override that projectile's direction
+    // (DynamicComponent), since by default the direction will always be the same - as it
+    // was loaded from YAML. We set the direction in spawnHook(), which is called by
+    // parent SpawnerProcess code after the prototype of the spawned entity has been
+    // constructed but still before spawning the entity.
+    immutable(WeaponMultiComponent)* currentWeapon_ = null;
+
+    import tharsis.defaults.components;
+    import tharsis.entity.componenttypemanager;
+    import tharsis.entity.entitymanager;
+    import tharsis.entity.entityprototype;
+    import tharsis.entity.resourcemanager;
+
+public:
+    /** Construct a WeaponizedSpawnerProcess.
+     *
+     * Params:
+     *
+     * addEntity            = Delegate to add an entity.
+     * prototypeManager     = Manages entity prototype resources.
+     * componentTypeManager = The component type manager where all used component types
+     *                        are registered.
+     * log                  = The game log.
+     *
+     * Examples:
+     * --------------------
+     * // EntityManager entityManager
+     * // ResourceManager!EntityPrototypeResource prototypeManager
+     * // ComponentTypeManager componentTypeManager
+     * // Logger log
+     * auto spawner = new WeaponizedSpawnerProcess(&entityManager.addEntity, prototypeManager,
+     *                                             componentTypeManager, log);
+     * --------------------
+     */
+    this(DefaultSpawnerProcess.AddEntity addEntity,
+         ResourceManager!EntityPrototypeResource prototypeManager,
+         AbstractComponentTypeManager componentTypeManager,
+         Logger log)
+        @safe pure nothrow
+    {
+        log_ = log;
+        super(addEntity, prototypeManager, componentTypeManager);
+    }
+
+    import std.typecons;
+    import tharsis.entity.componenttypeinfo;
+
+    override void spawnHook(ref EntityPrototype.GenericComponentRange!(No.isConst) components)
+        @system nothrow
+    {
+        const zero = vec3(0, 0, 0);
+        if(currentWeapon_ is null || currentWeapon_.firingDirection == zero) { return; }
+        // This is called after the prototype is locked; we can't add any components.
+        for(; !components.empty; components.popFront)
+        {
+            RawComponent* comp = &(components.front());
+            if(comp.typeID != DynamicComponent.ComponentTypeID) { continue; }
+
+            DynamicComponent* dynamic = &(comp.as!DynamicComponent());
+            // Keep the same velocity, but redirect it in firing direction.
+            const baseVelocity = vec3(dynamic.velocityX, dynamic.velocityY, dynamic.velocityZ);
+            const speed        = baseVelocity.length;
+            const velocity     = speed * currentWeapon_.firingDirection;
+
+            dynamic.velocityX = velocity.x;
+            dynamic.velocityY = velocity.y;
+            dynamic.velocityZ = velocity.z;
+        }
+    }
+
+    /// Shortcut alias.
+    alias Context = DefaultEntityManager.Context;
+
+    /** A simple forward for SpawnerProcess process().
+     *
+     * Needed because user code doesn't see SpawnerProcess.process() for some reason,
+     * probably a DMD bug (as of DMD 2.066).
+     */
+    override void process(ref const(Context) context,
+                          immutable SpawnerMultiComponent[] spawners,
+                          immutable TimedTriggerMultiComponent[] triggers) nothrow
+    {
+        super.process(context, spawners, triggers);
+    }
+
+    /** Reads spawners and weapons.
+     *
+     * If a weapon fires ($(D weapon.secsTillBurst < 0)), spawns projectiles of that
+     * weapon.
+     */
+    void process(ref const(Context) context,
+                 immutable SpawnerMultiComponent[] spawners,
+                 immutable WeaponMultiComponent[] weapons) nothrow
+    {
+        // Find triggers matching this spawner component, and spawn if found.
+        outer: foreach(ref spawner; spawners) foreach(size_t idx, ref weapon; weapons)
+        {
+            const weaponID = idx + minWeaponTriggerID;
+            // Weapon trigger must match the spawner component's trigger ID.
+            if(weaponID != spawner.triggerID) { continue; }
+
+            // If the spawner is not fully loaded (any of its resources not in the
+            // Loaded state), ignore it completely and move on to the next one. This
+            // means we miss spawns when a spawner is not loaded. We may add 'delayed'
+            // spawns to compensate for this in future.
+            if(!spawnerReady(spawner)) { continue outer; }
+
+            // We've not reached the time to spawn yet.
+            if(weapon.secsTillBurst <= 0.0f)
+            {
+                currentWeapon_ = &weapon;
+                spawn(context, spawner);
+                currentWeapon_ = null;
+            }
+        }
+    }
+
+    /// Process an entity with both weapons and timed triggers.
+    void process(ref const(Context) context,
+                 immutable SpawnerMultiComponent[] spawners,
+                 immutable WeaponMultiComponent[] weapons,
+                 immutable TimedTriggerMultiComponent[] triggers) nothrow
+    {
+        // Delegate to process() overloads handling individual triggers
+        // (weapons, timed triggers ATM).
+        process(context, spawners, weapons);
+        process(context, spawners, triggers);
+    }
+}
