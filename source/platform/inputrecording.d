0ef5ab7c84923dc6df4c2153b0317ce90dbe4112
Ferdinand Majerech <kiithsacmp@gmail.com>
2014-09-11 19:17:02 +0200

    Binary and YAML Recording implementations.

--- source/platform/inputrecording.d
+++ source/platform/inputrecording.d
@@ -167,3 +167,146 @@ pure nothrow @nogc:
     }
 }
 
+/** Recording that iterates over events recorded as binary data by a Recorder.
+ *
+ * Used for replaying recordings recorded during current game and for serializing
+ * recordings for writing to YAML.
+ */
+final class BinaryRecording(Input): Recording!Input
+{
+private:
+    alias Event = Recorder!Input.Event;
+
+    import std.container;
+
+    // Raw binary recording.
+    Array!ubyte data_;
+
+    // Range to the part of data_ for the remainder of the BinaryRecording range.
+    data_.Range dataRange_;
+
+package:
+nothrow:
+    /** Construct a BinaryRecording from recorded data.
+     *
+     * Only InputDevice code can construct a BinaryRecording.
+     *
+     * Params:
+     *
+     * data = Recorded data.
+     */
+    this(ref Array!ubyte data)
+        @trusted //pure @nogc
+    {
+        data_      = data.dup.assumeWontThrow;
+        dataRange_ = data_[].assumeWontThrow;
+        updateFront();
+    }
+
+protected:
+    override void popFront() @trusted //pure @nogc
+    {
+        assert(!empty, "Calling popFront() on an empty range");
+
+        // Skip current Event in binary data, and also skip BaseState after Change events.
+        const event = cast(Event)dataRange_.front;
+        if(event == Event.NoChange) { dataRange_.popFront(); }
+        else
+        {
+            dataRange_.popFront();
+            delegate
+            {
+                dataRange_ = dataRange_[Input.BaseState.sizeof .. $];
+            }().assumeWontThrow;
+        }
+        updateFront();
+    }
+
+    override bool empty() @safe pure const @nogc
+    {
+        return dataRange_.empty;
+    }
+
+private:
+    /// Update front of the range (inputState_). Called by constructor/popFront().
+    void updateFront() @trusted
+    {
+        if(empty) { return; }
+        // If current event is Change, read BaseState from binary data.
+        const event = cast(Event)dataRange_.front;
+        if(event != Event.NoChange)
+        {
+            assert(event == Event.Change, "unknown recorder event ID");
+            // `1` skips the event ID.
+            inputState_ = *cast(Input.BaseState*)&(dataRange_[1]);
+        }
+    }
+}
+
+/// Recording that reads recorded input from YAML.
+final class YAMLRecording(Input): Recording!Input
+{
+private:
+    alias Event = Recorder!Input.Event;
+
+    // Currently GC-based, but shouldn't be an issue as recordings are mostly for
+    // debugging, and even otherwise should be used infrequently (e.g. once per game,
+    // as opposed to once per some fixed time period)
+
+    // Recording events (Change, NoChange).
+    Event[] events_;
+    // States of recorded data to change to for every Change event in events_.
+    Input.BaseState[] states_;
+
+package:
+    /** Construct a YAMLRecording from a YAML node (produced by toYAML(Recording)).
+     *
+     * Throws:
+     *
+     * ConvException if any value in the YAML has unexpected format.
+     * YAMLException if the YAML has unexpected layout.
+     */
+    this(YAMLNode data) @trusted
+    {
+        foreach(string key, ref YAMLNode value; data)
+        {
+            events_.assumeSafeAppend;
+            events_ ~= to!Event(key);
+            if(events_.back == Event.NoChange) { continue; }
+            states_ ~= Input.BaseState.fromYAML(value);
+            states_.assumeSafeAppend;
+        }
+        updateFront();
+    }
+
+protected:
+@safe pure nothrow @nogc:
+    override void popFront()
+    {
+        assert(!empty, "Calling popFront() on an empty range");
+
+        const event = events_.front;
+        if(event != Event.NoChange)
+        {
+            assert(event == Event.Change, "unknown recorder event ID");
+            states_.popFront();
+        }
+        events_.popFront();
+        updateFront();
+    }
+
+    override bool empty() const { return events_.empty; }
+
+private:
+    /// Update front of the range (inputState_). Called by constructor/popFront().
+    void updateFront()
+    {
+        if(empty) { return; }
+        const event = events_.front;
+        if(event != Event.NoChange)
+        {
+            assert(event == Event.Change, "unknown recorder event ID");
+            inputState_ = states_.front;
+        }
+    }
+}
