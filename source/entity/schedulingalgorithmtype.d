d6c7517196cf81c5e051ef96f57ffcafceac59fe
Ferdinand Majerech <kiithsacmp@gmail.com>
2014-12-17 11:39:47 +0100

    Added enumeration of Tharsis schedulers.

---
+++ source/entity/schedulingalgorithmtype.d
@@ -0,0 +1,24 @@
+//          Copyright Ferdinand Majerech 2014.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+module entity.schedulingalgorithmtype;
+
+
+/// Enumerates all scheduling algorithm 'types' we can use with THarsis.
+enum SchedulingAlgorithmType
+{
+    /// Longest Processing Time. This is the default scheduling algorithm type (.init).
+    LPT,
+    /// Dumb - equal number of Processes in each thread.
+    Dumb,
+    /// Bruteforce - slow (SLOW) backtracking.
+    BRUTE,
+    /// Randomized backtracking: timelimit = 400, attempts=3
+    RBt400r3,
+    /// Randomized backtracking: timelimit = 800, attempts=6
+    RBt800r6,
+    /// Randomized backtracking: timelimit = 1600, attempts=9
+    RBt1600r9
+}
