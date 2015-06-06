69f7bf91631cd071d48fb7c1d7040d0c8b2401d5
Ferdinand Majerech <kiithsacmp@gmail.com>
2015-06-06 10:28:31 +0200

    Refactored CLI into more general Config

--- source/app.d
+++ source/app.d
@@ -3,6 +3,7 @@ import std.array;
 import std.conv;
 import std.exception;
 import std.experimental.logger;
+import std.getopt;
 import std.stdio;
 import std.string;
 import std.typecons;
@@ -28,7 +29,7 @@ import time.gametime;
 // TODO: Move fixedFPS to YAML 2014-09-12
 /** We use fixed effective FPS and time step to make game updates more 'discrete'.
  *
- * If we can't maintain the FPS, the game slows down (hence we need to do everything to 
+ * If we can't maintain the FPS, the game slows down (hence we need to do everything to
  * keep overhead low enough to keep this FPS).
  */
 // Kernel distorts results too much at 60FPS, go with 50 for now.
@@ -39,30 +40,6 @@ enum fixedFPS = 50.0f;
 
 
 
-/// Exception thrown at CLI errors.
-class CLIException : Exception 
-{
-    public this(string msg, string file = __FILE__, int line = __LINE__)
-    {
-        super(msg, file, line);
-    }
-}
-
-/** Process a command line option (argument starting with --).
- *
- * Params:  arg     = Argument to process.
- *          process = Function to process the option. Takes
- *                    the option and its arguments.
- * Throws:  CLIException if arg is not an option, and anything process() throws.
- *
- */
-void processOption(string arg, void delegate(string, string[]) process)
-{
-    enforce(arg.startsWith("--"), new CLIException("Unknown argument: " ~ arg));
-    auto argParts = arg[2 .. $].split("=");
-    process(argParts[0], argParts[1 .. $]);
-}
-
 /// Print help information.
 void help()
 {
@@ -72,7 +49,7 @@ void help()
         "Benchmark game for Tharsis",
         "Copyright (C) 2014 Ferdinand Majerech",
         "",
-        "Usage: tharsis-game [--help] <command> [local-options ...]",
+        "Usage: tharsis-game [command] [options ...]",
         "",
         "Global options:",
         "  --help                     Print this help information.",
@@ -118,235 +95,209 @@ void help()
 }
 
 
-/** Command-line interface.
+/** Raw configuration data from the command-line and YAML.
  *
- * Works with git-style commands (currently only 'demo', which runs the game with input
- * from an input record file).
- *
- * If there is no command, the game is launched normally.
+ * Note that the GeneralConfig constructor removes the command-line arguments it parses.
  */
-struct CLI
+struct ConfigData
 {
-private:
-    /* Current command line argument processing function.
+    /** CLI arguments passed to the program.
      *
-     * In the beginning, this is the function to process global arguments. When a command
-     * is encountered, it is set to that command's local arguments parser function.
+     * Note: GeneralConfig constructor removes 'general config' arguments from here.
      */
-    void delegate(string) processArg_;
-    // Action to execute (determined by command line arguments)
-    int delegate() action_;
+    string[] cliArgs;
+    // TODO: YAMLNode yaml
+}
 
-    // Options/arguments for the 'demo' command.
-    struct Demo
+
+// Main pattern for Tharsis configs: a struct with a ctor to initialize it from
+// ConfigData
+
+/// Options/arguments common for all commands.
+struct GeneralConfig
+{
+    // The number of threads for Tharsis to use. If 0, the number will be autodetected.
+    uint threadCount = 0;
+
+    // Are we running headless (without video output) ?
+    bool headless;
+
+    // Scheduling algorithm to use (in Tharsis) from start (.init is intentionally LPT).
+    SchedulingAlgorithmType schedAlgo = SchedulingAlgorithmType.init;
+
+    // Window/camera width to start with (affects even headless runs).
+    uint width = 1024;
+
+    // Window/camera height to start with (affects even headless runs).
+    uint height = 768;
+
+    // "game", "demo" or "help"
+    string command = "game";
+
+    /** Parse GeneralConfig data from a ConfigData.
+     *
+     * Note: removes 'general config' arguments from ConfigData.cliArgs.
+     */
+    this(ref ConfigData data)
     {
-        // Name of the recorded input filename for the 'demo' command.
-        string inputName_;
-
-        // Should direct mouse/keyboard input be blocked when replaying the demo?
-        Flag!"block" blockInput_ = Yes.block;
-
-        // Should the game quit when the the replay is finished?
-        Flag!"quitWhenDone" quitWhenDone_ = No.quitWhenDone;
-    }
-
-    // Options/arguments common for all commands.
-    struct Args
-    {
-        // The number of threads for Tharsis to use. If 0, the number will be autodetected.
-        uint threadCount = 0;
-
-        // Are we running headless (without video output) ?
-        Flag!"headless" headless;
-
-        // Scheduling algorithm to use (in Tharsis) from start (.init is intentionally LPT).
-        SchedulingAlgorithmType schedAlgo = SchedulingAlgorithmType.init;
-
-        // Window/camera width to start with (affects even headless runs).
-        uint width = 1024;
-
-        // Window/camera height to start with (affects even headless runs).
-        uint height = 768;
-    }
-
-    // Options/arguments for the 'demo' command.
-    Demo demo_;
-
-    // Options/arguments common for all commands.
-    Args args_;
-
-public:
-    /// Construct a CLI with specified command-line arguments and parse them.
-    this(string[] cliArgs)
-    {
-        // The 'default command' - run the game.
-        action_ = ()
+        try
         {
+            auto cliArgs = data.cliArgs[];
+            if(cliArgs.length >= 2 && !cliArgs[1].startsWith("-"))
+            {
+                command = data.cliArgs[1];
+                cliArgs = cliArgs[0] ~ cliArgs[2 .. $];
+            }
+
+            bool help;
+            getopt(cliArgs,
+                   std.getopt.config.passThrough,
+                   std.getopt.config.bundling,
+                   "help", &help, "headless", &headless, "sched-algo", &schedAlgo,
+                   "width", &width, "height", &height, "threadCount", &threadCount);
+            data.cliArgs = cliArgs;
+
+            if(help) { command = "help"; }
+        }
+        catch(Exception e)
+        {
+            writeln("Warning: Failed to parse CLI args in GeneralConfig constructor");
+        }
+    }
+}
+
+/// Options/arguments for the 'demo' command.
+struct DemoConfig
+{
+    // Name of the recorded input filename for the 'demo' command.
+    string inputName_;
+
+    // Should direct mouse/keyboard input be blocked when replaying the demo?
+    Flag!"block" blockInput_ = Yes.block;
+
+    // Should the game quit when the the replay is finished?
+    Flag!"quitWhenDone" quitWhenDone_ = No.quitWhenDone;
+
+    this(ref ConfigData data)
+    {
+        bool directInput;
+        string[] cliArgs = data.cliArgs;
+        try
+        {
+            getopt(cliArgs,
+                   std.getopt.config.passThrough,
+                   std.getopt.config.bundling,
+                   "direct", &directInput, "quitWhenDone", &quitWhenDone_);
+        }
+        catch(Exception e)
+        {
+            writeln("Warning: Failed to parse CLI args in DemoConfig constructor");
+        }
+
+        blockInput_ = directInput ? No.block : Yes.block;
+
+        // the [1 .. $] skips the binary name
+        foreach(arg; cliArgs[1 .. $].filter!(arg => !arg.startsWith("-")))
+        {
+            if(inputName_ !is null)
+            {
+                inputName_ = null;
+                writeln("ERROR: `demo` can have only one argument: input file name");
+                return;
+            }
+            inputName_ = arg;
+        }
+        if(inputName_ is null)
+        {
+            writeln("ERROR: Demo file name not specified");
+        }
+    }
+}
+
+/// Execute the game. This is the 'real main()'.
+int execute(string[] cliArgs)
+{
+    auto cfgData = ConfigData(cliArgs);
+    const cfg = GeneralConfig(cfgData);
+    switch(cfg.command)
+    {
+        case "game":
             // For now. Should log to an in-memory buffer later.
             auto log = stdlog;
 
             if(!loadDerelict(log)) { return 1; }
             scope(exit)            { unloadDerelict(); }
-            if(!initSDL(log, args_.headless)) { return 1; }
-            scope(exit)       { SDL_Quit(); }
+            if(!initSDL(log, cfg.headless)) { return 1; }
+            scope(exit)            { SDL_Quit(); }
 
+            auto video = cfg.headless ? null : new VideoDevice(log);
+            scope(exit) if(!cfg.headless) { video.destroy(); }
+            if(!cfg.headless && !initVideo(cfg, video, log)) { return 1; }
 
-            auto video = args_.headless ? null : new VideoDevice(log);
-            scope(exit) if(!args_.headless) { video.destroy(); }
-            if(!args_.headless && !initVideo(video, log)) { return 1; }
-
-            auto input = scoped!InputDevice(() => args_.headless ? args_.height : video.height,
-                                            log);
+            auto input = scoped!InputDevice(() => cfg.headless ? cfg.height : video.height, log);
             auto gameTime = scoped!GameTime(1 / fixedFPS);
 
-            runGame(video, input, gameTime, args_, log);
-
+            runGame(video, input, gameTime, cfg, log);
             return 0;
-        };
-        // We start parsing global options/commands.
-        processArg_ = &globalOrCommand;
-        foreach(arg; cliArgs[1 .. $]) { processArg_(arg); }
-    }
 
-    /// Execute the action specified by command line arguments.
-    int execute()
-    {
-        // Execute the command.
-        try
-        {
-            return action_();
-        }
-        catch(CLIException e) { writeln("Command-line error: ", e.msg); help(); }
-        catch(Throwable e)    { writeln("Unhandled error: ", e); }
-        return 1;
-    }
+        case "demo":
+            const democfg = DemoConfig(cfgData);
+            if(democfg.inputName_ is null)
+            {
+                return 1;
+            }
 
-private:
-    // Parses local options for the "top" command.
-    void localDemo(string arg)
-    {
-        if(!arg.startsWith("--"))
-        {
-            enforce(demo_.inputName_ is null,
-                    new CLIException("`demo` can have only one argument: input file name"));
-            demo_.inputName_ = arg;
-            return;
-        }
+            // For now. Should log to an in-memory buffer later.
+            auto log = stdlog;
 
-        processOption(arg, (opt, args) {
+            if(!loadDerelict(log))          { return 1; }
+            scope(exit)                     { unloadDerelict(); }
+            if(!initSDL(log, cfg.headless)) { return 1; }
+            scope(exit)                     { SDL_Quit(); }
 
-        switch(opt)
-        {
-            case "direct":       demo_.blockInput_ = No.block;           break;
-            case "quitWhenDone": demo_.quitWhenDone_ = Yes.quitWhenDone; break;
-            default: throw new CLIException("Unrecorgnized demo option: --" ~ opt);
-        }
+            auto video = cfg.headless ? null : new VideoDevice(log);
+            scope(exit) if(!cfg.headless) { video.destroy(); }
+            if(!cfg.headless && !initVideo(cfg, video, log)) { return 1; }
 
-        });
-    }
+            auto input = scoped!InputDevice(() => cfg.headless ? cfg.height : video.height, log);
+            auto gameTime = scoped!GameTime(1 / fixedFPS);
 
-    // Parse a command. Sets up command state and switches to its option parser function.
-    void command(string arg)
-    {
-        switch(arg)
-        {
-            // The 'demo' command: run the game, replaying input from a file.
-            //
-            // Filename is specified by an argument (see localDemo()).
-            case "demo": 
-                processArg_ = &localDemo; 
-                action_ = ()
-                {
-                    enforce(demo_.inputName_ !is null,
-                            new CLIException("Demo file name not specified"));
+            // Load recorded input.
+            import io.yaml;
+            try
+            {
+                auto replay = Loader(democfg.inputName_).load();
+                input.replayFromYAML(replay, democfg.blockInput_, democfg.quitWhenDone_);
+            }
+            catch(Exception e)
+            {
+                log.warning("Failed to load input recording").assumeWontThrow;
+            }
 
-                    // For now. Should log to an in-memory buffer later.
-                    auto log = stdlog;
-
-                    if(!loadDerelict(log))            { return 1; }
-                    scope(exit)                       { unloadDerelict(); }
-                    if(!initSDL(log, args_.headless)) { return 1; }
-                    scope(exit)                       { SDL_Quit(); }
-
-                    auto video = args_.headless ? null : new VideoDevice(log);
-                    scope(exit) if(!args_.headless) { video.destroy(); }
-                    if(!args_.headless && !initVideo(video, log)) { return 1; }
-
-                    auto input = scoped!InputDevice(() => args_.headless ? args_.height 
-                                                                         : video.height,
-                                                    log);
-                    auto gameTime = scoped!GameTime(1 / fixedFPS);
-
-                    // Load recorded input.
-                    import io.yaml;
-                    try
-                    {
-                        auto replay = Loader(demo_.inputName_).load();
-                        input.replayFromYAML(replay, demo_.blockInput_, demo_.quitWhenDone_);
-                    }
-                    catch(Exception e)
-                    {
-                        log.warning("Failed to load input recording").assumeWontThrow;
-                    }
-
-                    runGame(video, input, gameTime, args_, log);
-
-                    return 0;
-                };
-                break;
-            default: throw new CLIException("Unknown command: " ~ arg);
-        }
-    }
-
-    /// Parse a global option or command.
-    void globalOrCommand(string arg)
-    {
-        // Command
-        if(!arg.startsWith("-")) 
-        {
-            command(arg);
-            return;
-        }
-
-        // Global option
-        processOption(arg, (opt, args){
-        try switch(opt)
-        {
-            case "help": 
-                help();
-                action_ = () { return 0; };
-                return;
-            case "sched-algo": args_.schedAlgo   = to!SchedulingAlgorithmType(args[0]); break;
-            case "headless":   args_.headless    = Yes.headless;                        break;
-            case "threads":    args_.threadCount = to!uint(args[0]);                    break;
-            case "width":      args_.width       = max(1, to!uint(args[0]));            break;
-            case "height":     args_.height      = max(1, to!uint(args[0]));            break;
-            default: throw new CLIException("Unrecognized global option: " ~ opt);
-        }
-        catch(ConvException e)
-        {
-            writefln("Invalid argument/s for option '--%s': '%s'", opt, args);
+            runGame(video, input, gameTime, cfg, log);
+            return 0;
+        case "help":
             help();
-            action_ = () { return 0; };
-        }
-        });
-    }
-
-
-    /// Initialize the video device (setting video mode and initializing OpenGL).
-    bool initVideo(VideoDevice video, Logger log)
-    {
-        // Initialize the video device.
-        const width        = args_.width;
-        const height       = args_.height;
-        const fullscreen   = No.fullscreen;
-
-        if(!video.initWindow(width, height, fullscreen)) { return false; }
-        if(!video.initGL()) { return false; }
-        return true;
+            return 0;
+        default:
+            writeln("ERROR: unknown command '", cfg.command, "'");
+            return 1;
     }
 }
 
+
+/// Initialize the video device (setting video mode and initializing OpenGL).
+bool initVideo(ref const GeneralConfig cfg, VideoDevice video, Logger log)
+{
+    // Initialize the video device.
+    const width        = cfg.width;
+    const height       = cfg.height;
+    const fullscreen   = No.fullscreen;
+
+    if(!video.initWindow(width, height, fullscreen)) { return false; }
+    if(!video.initGL()) { return false; }
+    return true;
+}
 
 /// Load libraries using through Derelict (currently, this is SDL2).
 bool loadDerelict(Logger log)
@@ -379,7 +330,7 @@ void unloadDerelict()
  * log      = Game log.
  * headless = Are we running without video output? (If so, don't init the video subsystem).
  */
-bool initSDL(Logger log, Flag!"headless" headless)
+bool initSDL(Logger log, bool headless)
 {
     // Initialize SDL Video subsystem.
     if(SDL_Init(headless ? 0 : SDL_INIT_VIDEO) < 0)
@@ -408,11 +359,11 @@ void deinitSDL()
  * log      = Game log.
  */
 int runGame(VideoDevice video, InputDevice input, GameTime gameTime,
-            ref const CLI.Args args, Logger log)
+            ref const GeneralConfig cfg, Logger log)
 {
     // TODO: We should use D:GameVFS to access files, with a custom YAML source reading
     //       files through D:GameVFS. 2014-08-27
-    auto camera        = new Camera(cast(size_t)args.width, cast(size_t)args.height);
+    auto camera        = new Camera(cast(size_t)cfg.width, cast(size_t)cfg.height);
     auto cameraControl = new CameraControl(gameTime, input, camera, log);
 
     // Initialize the main profiler (used to profile both the game and Tharsis).
@@ -426,8 +377,8 @@ int runGame(VideoDevice video, InputDevice input, GameTime gameTime,
     import tharsis.entity.scheduler;
 
     // One profiler per thread.
-    const threadCount = args.threadCount == 0 ? autodetectThreadCount()
-                                              : args.threadCount;
+    const threadCount = cfg.threadCount == 0 ? autodetectThreadCount()
+                                             : cfg.threadCount;
     void[][] profBuffers;
     Profiler[] profilers;
     profBuffers ~= AlignedMallocator.it.alignedAllocate(profSpaceMainThread, 64);
@@ -442,9 +393,9 @@ int runGame(VideoDevice video, InputDevice input, GameTime gameTime,
     {
         AlignedMallocator.it.deallocate(buffer);
     }
-    auto entitySystem = EntitySystem(video, input, gameTime, camera, args.threadCount, 
+    auto entitySystem = EntitySystem(video, input, gameTime, camera, cfg.threadCount,
                                      profilers, log);
-    entitySystem.schedulingAlgorithm = args.schedAlgo;
+    entitySystem.schedulingAlgorithm = cfg.schedAlgo;
     scope(failure) { log.critical("Unexpected failure in the main loop"); }
 
     // Run the game itself.
@@ -505,19 +456,18 @@ int runGame(VideoDevice video, InputDevice input, GameTime gameTime,
 
 /** Program entry point.
  *
- * Rus the CLI and catches any uncaught throwables.
+ * Catches any uncaught throwables.
  */
 int main(string[] args)
 {
-    try { return CLI(args).execute(); }
+    try
+    {
+        return execute(args);
+    }
     catch(ConvException e)
     {
         writeln("String conversion error. Maybe a command-line has incorrect format?\n",
                 "error: ", e.msg);
-    }
-    catch(CLIException e)
-    {
-        writeln("Command-line error: ", e);
     }
     catch(Throwable e)
     {
